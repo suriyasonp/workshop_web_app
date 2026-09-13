0cc43a1cd4c2240f9e1a7ddf566c8c5a00b6d5ab
mo'sy <sutthiphat.su@ku.th>
2026-09-13 06:03:58 +0100

    Mapput endpoint [dev] endpoint

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -42,4 +42,25 @@ app.MapPost("/api/todos", (TodoPostDto dto) =>
     return Results.Created($"/api/todos/{todo.Id}", todo);
 });
 
+app.MapPut("/api/todos/{id}", (int id, TodoPutDto dto) =>
+{
+    try
+    {
+        var index = todos.FindIndex(t => t.Id == id);
+        if (index == -1) return Results.NotFound();
+
+        todos[index] = todos[index] with
+        {
+            Title = dto.Title,
+            IsCompleted = dto.IsCompleted
+        };
+    
+        return Results.NotFound();
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+
+});
 app.Run();
